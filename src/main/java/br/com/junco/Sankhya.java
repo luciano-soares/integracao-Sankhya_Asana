ce928a6fd0b28d090a4cd4e919a63f7c344f7a0d
Luci <soaresluciano85@gmail.com>
2024-10-07 20:45:31 -0300

    Add: Login adicionado na classe sankhya

---
+++ src/main/java/br/com/junco/Sankhya.java
@@ -0,0 +1,91 @@
+package br.com.junco;
+
+import okhttp3.*;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Sankhya {
+    private String usuario;
+    private String senha;
+    private String token;
+    private String appKey;
+    private String bearerToken;
+
+    public Sankhya(String usuario, String senha, String token, String appKey){
+        this.usuario = usuario;
+        this.senha = senha;
+        this.token = token;
+        this.appKey = appKey;
+    }
+
+    public boolean login() throws IOException, JSONException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = RequestBody.create("", mediaType);
+        Request request = new Request.Builder()
+                .url("https://api.sankhya.com.br/login")
+                .method("POST", body)
+                .addHeader("token", getToken())
+                .addHeader("appkey", getAppKey())
+                .addHeader("username", getUsuario())
+                .addHeader("password", getSenha())
+                .build();
+        try{
+            Response response = client.newCall(request).execute();
+            JSONObject responseBody = new JSONObject(response.body().string());
+            System.out.println(responseBody);
+            System.out.println(responseBody.getString("error"));
+
+            if (Objects.equals(responseBody.getString("error"), "null")){
+                setBearerToken(responseBody.getString("bearerToken"));
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        catch (Exception e){
+            return false;
+        }
+    }
+
+    private String getUsuario() {
+        return usuario;
+    }
+    private void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    private String getSenha() {
+        return senha;
+    }
+    private void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    private String getToken() {
+        return token;
+    }
+    private void setToken(String token) {
+        this.token = token;
+    }
+
+    private String getAppKey() {
+        return appKey;
+    }
+    private void setAppKey(String appKey) {
+        this.appKey = appKey;
+    }
+
+    private void setBearerToken(String bearerToken) {
+        this.bearerToken = bearerToken;
+    }
+
+    private String getBearerToken() {
+        return bearerToken;
+    }
+}
