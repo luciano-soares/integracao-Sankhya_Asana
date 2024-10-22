f6d7718400f0c1e82986487d68ded9ddf51560a4
Luci <soaresluciano85@gmail.com>
2024-10-21 22:26:39 -0300

    Add: Método para criação de tarefas criado na classe 'Asana'

--- src/main/java/br/com/junco/Asana.java
+++ src/main/java/br/com/junco/Asana.java
@@ -15,6 +15,43 @@ public class Asana {
         this.token = token;
     }
 
+    public void createTask(String titulo, String assignee, String dueDate,String descrition, String notes, String section){
+        try{
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            MediaType mediaType = MediaType.parse("application/json");
+            String bodyString = "{" +
+                    "\"data\": {" +
+                    "\"workspace\": \"" + workspace + "\"," +
+                    "\"name\": \"" + titulo + "\"," +
+                    "\"assignee\":  \"" + assignee + "\"," +
+                    "\"due_at\": \"" + dueDate + "\"," +
+                    "\"html_notes\": \"" + descrition + "\"," +
+                    "\"notes\": \"" + notes + "\"," +
+                    "\"assignee_section\": \"" + section + "\"," +
+                    "\"projects\": " +
+                    "[\"1207282158936107\"" +
+                    "]" +
+                    "}" +
+                    "}";
+            System.out.println(bodyString);
+            RequestBody body    = RequestBody.create(bodyString, mediaType);
+            System.out.println(bodyString);
+            Request request = new Request.Builder()
+                    .url("https://app.asana.com/api/1.0/tasks")
+                    .method("POST", body)
+                    .addHeader("accept", "application/json")
+                    .addHeader("content-type", "application/json")
+                    .addHeader("Authorization", "Bearer " + getToken())
+                    .build();
+            Response response = client.newCall(request).execute();
+            System.out.println(response.body().string());
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
     private void setToken(String token) {
         this.token = token;
     }
